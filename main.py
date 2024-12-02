10a595945ab59bf7037ec0c0066c29cf17952745
Porog <anatolijmordvin31@gmail.com>
2024-12-02 17:12:51 +0300

    Новый взгляд

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Circles(QWidget, Ui_Form):
         qp.drawEllipse(self.point, self.radius, self.radius)
         qp.drawEllipse(self.point, self.radius // 2, self.radius // 2)
         qp.setBrush(self.color)
-        qp.drawEllipse(self.point, self.radius // 2, self.radius)
+        qp.drawEllipse(self.point, self.radius // 4, self.radius // 2)
 
     def paintEvent(self, event):
         if self.f:
